fccdbc3025f3e45f30ead6656c2f756898527417
Rahul Verma <73533578+rahulverma1618@users.noreply.github.com>
2023-11-24 09:37:06 +0530

    Update ytdl.js

--- ytdl.js
+++ ytdl.js
@@ -1,27 +1,6 @@
-const ytdl = require('ytdl-core');
-const fs = require('fs');
+const ytdl = require('ytdl-core'); 
+const fs = require('fs') 
 
-const videoURL = 'https://youtu.be/AX6OrbgS8lI?si=aN4g4hRQPXdqp7wp';
+let url = "https://youtu.be/AX6OrbgS8lI?si=aN4g4hRQPXdqp7wp";
+ytdl(url).pipe(fs.createWriteStream('video.mp4')); 
 
-ytdl.getInfo(videoURL, (err, info) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-
-  if (!videoFormat) {
-    console.error('No suitable format found');
-    return;
-  }
-
-  const output = fs.createWriteStream('video.mp4');
-
-  ytdl(videoURL, { format: videoFormat })
-    .pipe(output);
-
-  output.on('finish', () => {
-    console.log('Download complete');
-  });
-});
