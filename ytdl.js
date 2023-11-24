8db68cbcfa2a790901901795d5c6afdddf7c7505
Rahul Verma <73533578+rahulverma1618@users.noreply.github.com>
2023-11-24 10:25:31 +0530

    Delete ytdl.js

--- ytdl.js
+++
@@ -1,6 +0,0 @@
-const ytdl = require('ytdl-core'); 
-const fs = require('fs') 
-
-let url = "https://youtu.be/AX6OrbgS8lI?si=aN4g4hRQPXdqp7wp";
-ytdl(url).pipe(fs.createWriteStream('video.mp4')); 
-
